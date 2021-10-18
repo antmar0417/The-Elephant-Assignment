01f67d263863d36820ee429902fc759ea63c532b
antmar0417 <tonyboy@live.se>
2021-10-18 19:45:11 +0200

    added data.php file

---
+++ data.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/functions.php';
+
+$cars = [
+    ['make' => 'Ford', 'model' => 'Focus', 'Year' => 2005, 'kw' => 100, 'vinId' => 'JT164STJ200017216', 'color' => 'Blue'],
+    ['make' => 'Volvo', 'model' => 'V70', 'Year' => 2008, 'kw' => 110, 'vinId' => 'JT164STJ210017216', 'color' => 'Silver'],
+    ['make' => 'Ford', 'model' => 'Fiesta', 'Year' => 2010, 'kw' => 62, 'vinId' => 'JT164STJ220017216', 'color' => 'Red'],
+    ['make' => 'Ford', 'model' => 'Mondeo', 'Year' => 2009, 'kw' => 120, 'vinId' => 'JT164STJ230017216', 'color' => 'Black'],
+    ['make' => 'Ford', 'model' => 'F150', 'Year' => 2013, 'kw' => 202, 'vinId' => 'JT164STJ240017216', 'color' => 'Yellow'],
+    ['make' => 'Audi', 'model' => 'A4', 'Year' => 2018, 'kw' => 150, 'vinId' => 'JT164STJ250017216', 'color' => 'Green'],
+    ['make' => 'Volkswagen', 'model' => 'Tiguan', 'Year' => 2012, 'kw' => 121, 'vinId' => 'JT164STJ260017216', 'color' => 'Red'],
+    ['make' => 'Volkswagen', 'model' => 'Polo', 'Year' => 2005, 'kw' => 100, 'vinId' => 'JT164STJ270017216', 'color' => 'Blue'],
+];
+
+$owners = [
+    ['firstName' => 'Barry', 'lastName' => 'Johnson', 'phoneNumber' => '0700555555'],
+    ['firstName' => 'Liam', 'lastName' => 'Brown', 'phoneNumber' => '0700555556'],
+    ['firstName' => 'Emma', 'lastName' => 'Garcia', 'phoneNumber' => '0700555557'],
+    ['firstName' => 'Charlotte', 'lastName' => 'Davis', 'phoneNumber' => '070055558'],
+    ['firstName' => 'Oliver', 'lastName' => 'Williams', 'phoneNumber' => '0700555559'],
+];
