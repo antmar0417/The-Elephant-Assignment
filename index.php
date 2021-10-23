38c92d2af4fe0c1d2adf4279d618d0d34454509c
antmar0417 <tonyboy@live.se>
2021-10-23 17:38:35 +0200

    added content into index.php

--- index.php
+++ index.php
@@ -1,1 +1,40 @@
-<?= 'The Elephpent in the Room'; ?>
+<?php require __DIR__ . '/header.php'; ?>
+<?php require __DIR__ . '/functions.php'; ?>
+<main>
+    <section>
+        <h1 class="site-title">Cars.com</h1>
+        <p>Välj märke och bilmodell nedan.</p>
+        <div class="four-column-wrapper">
+            <p>
+                <a href="https://en.wikipedia.org/wiki/Big_cat">Audi</a>&nbsp;(1st)
+            </p>
+            <p>
+                <a href="https://en.wikipedia.org/wiki/Big_cat">Ford</a>&nbsp;(3st)
+            </p>
+            <p>
+                <a href="https://en.wikipedia.org/wiki/Big_cat">Volvo</a>&nbsp;(1st)
+            </p>
+            <p>
+                <a href="https://en.wikipedia.org/wiki/Big_cat">Volkswagen</a>&nbsp;(1st)
+            </p>
+        </div>
+    </section>
+    <h2>Bilar till salu</h2>
+    <section class="cars">
+        <?php foreach ($cars as $car) : ?>
+            <div class="car">
+                <div class="two-column-wrapper">
+                    <div class="image-wrapper">
+                        <img src="<?php echo $car['image'] ?>" alt="<?php echo $car['make'] . $car['model'] ?>" />
+                    </div>
+                    <ul>
+                        <li><?php echo $car['make'] . " " . $car['model'] ?></li>
+                        <li>Hästkrafter: <?php kwToHorsepower($car['kw']); ?></li>
+                        <p class="price">Pris: <?php echo $car['price'] ?></p>
+                    </ul>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </section>
+</main>
+<?php require __DIR__ . '/footer.php'; ?>
