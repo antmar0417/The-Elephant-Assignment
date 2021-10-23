1bbc1ecc5eabd05a07fd17db566dc24ff4d67e5e
antmar0417 <tonyboy@live.se>
2021-10-23 19:10:19 +0200

    added a dynamic title into header.php

--- header.php
+++ header.php
@@ -1,10 +1,11 @@
+<?php require __DIR__ . '/data.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Cars for sale</title>
+    <title><?php echo $title; ?></title>
     <link rel="stylesheet" href="style.css" />
 </head>
 
