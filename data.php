7a2354ef04dc54327c1bd22bbcb0e7f3f6c7933f
antmar0417 <tonyboy@live.se>
2021-10-23 17:32:18 +0200

    added price into cars

--- data.php
+++ data.php
@@ -2,17 +2,15 @@
 
 declare(strict_types=1);
 
-require __DIR__ . '/functions.php';
-
 $cars = [
-    ['make' => 'Ford', 'model' => 'Focus', 'Year' => 2005, 'kw' => 100, 'vinId' => 'JT164STJ200017216', 'color' => 'Blue', 'image' => 'images/Ford Focus.jpeg'],
-    ['make' => 'Volvo', 'model' => 'V70', 'Year' => 2008, 'kw' => 110, 'vinId' => 'JT164STJ210017216', 'color' => 'Silver', 'image' => 'images/Volvo V70.jpeg'],
-    ['make' => 'Ford', 'model' => 'Fiesta', 'Year' => 2010, 'kw' => 62, 'vinId' => 'JT164STJ220017216', 'color' => 'Red', 'image' => 'images/Ford Fiesta.jpeg'],
-    ['make' => 'Ford', 'model' => 'Mondeo', 'Year' => 2009, 'kw' => 120, 'vinId' => 'JT164STJ230017216', 'color' => 'Black', 'image' => 'images/Ford Mondeo.jpeg'],
-    ['make' => 'Ford', 'model' => 'F150', 'Year' => 2013, 'kw' => 202, 'vinId' => 'JT164STJ240017216', 'color' => 'Yellow', 'image' => 'images/Ford F150.jpeg'],
-    ['make' => 'Audi', 'model' => 'A4', 'Year' => 2018, 'kw' => 150, 'vinId' => 'JT164STJ250017216', 'color' => 'Green', 'image' => 'images/Audi A4.jpeg'],
-    ['make' => 'Volkswagen', 'model' => 'Tiguan', 'Year' => 2019, 'kw' => 121, 'vinId' => 'JT164STJ260017216', 'color' => 'Red', 'image' => 'images/VW Tiguan.jpeg'],
-    ['make' => 'Volkswagen', 'model' => 'Polo', 'Year' => 2018, 'kw' => 100, 'vinId' => 'JT164STJ270017216', 'color' => 'Blue', 'image' => 'images/VW Polo.jpeg'],
+    ['make' => 'Ford', 'model' => 'Focus', 'Year' => 2005, 'kw' => 100, 'vinId' => 'JT164STJ200017216', 'color' => 'Blue', 'image' => 'images/Ford Focus.jpeg', 'price' => 15000],
+    ['make' => 'Volvo', 'model' => 'V70', 'Year' => 2008, 'kw' => 110, 'vinId' => 'JT164STJ210017216', 'color' => 'Silver', 'image' => 'images/Volvo V70.jpeg', 'price' => 70000],
+    ['make' => 'Ford', 'model' => 'Fiesta', 'Year' => 2010, 'kw' => 62, 'vinId' => 'JT164STJ220017216', 'color' => 'Red', 'image' => 'images/Ford Fiesta.jpeg', 'price' => 30000],
+    ['make' => 'Ford', 'model' => 'Mondeo', 'Year' => 2009, 'kw' => 120, 'vinId' => 'JT164STJ230017216', 'color' => 'Black', 'image' => 'images/Ford Mondeo.jpeg', 'price' => 35000],
+    ['make' => 'Ford', 'model' => 'F150', 'Year' => 2013, 'kw' => 202, 'vinId' => 'JT164STJ240017216', 'color' => 'Yellow', 'image' => 'images/Ford F150.jpeg', 'price' => 120000],
+    ['make' => 'Audi', 'model' => 'A4', 'Year' => 2018, 'kw' => 150, 'vinId' => 'JT164STJ250017216', 'color' => 'Green', 'image' => 'images/Audi A4.jpeg', 'price' => 90000],
+    ['make' => 'Volkswagen', 'model' => 'Tiguan', 'Year' => 2019, 'kw' => 121, 'vinId' => 'JT164STJ260017216', 'color' => 'Red', 'image' => 'images/VW Tiguan.jpeg', 'price' => 190000],
+    ['make' => 'Volkswagen', 'model' => 'Polo', 'Year' => 2018, 'kw' => 100, 'vinId' => 'JT164STJ270017216', 'color' => 'Blue', 'image' => 'images/VW Polo.jpeg', 'price' => 170000],
 ];
 
 $owners = [
@@ -22,12 +20,3 @@ $owners = [
     ['firstName' => 'Charlotte', 'lastName' => 'Davis', 'phoneNumber' => '070055558'],
     ['firstName' => 'Oliver', 'lastName' => 'Williams', 'phoneNumber' => '0700555559'],
 ];
-
-?>
-
-<h3>Cars into array</h3>
-<ol>
-    <?php foreach ($cars as $car) : ?>
-        <li><img src="<?php echo $car['image'] ?>" alt="car"><?php echo $car['make'] . " " . $car['model'] . " has: " . kwToHorsepower($car['kw']) . " horsepower"; ?></li>
-    <?php endforeach; ?>
-</ol>
