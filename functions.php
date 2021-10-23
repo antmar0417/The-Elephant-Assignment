d8df111fc261c95bceaf1a57497e1c65d0f5bcc7
antmar0417 <tonyboy@live.se>
2021-10-23 19:04:09 +0200

    removed require file dclaration into functions.php

--- functions.php
+++ functions.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 function kwToHorsepower(float $power): float
 {
-    $power = floatval(number_format($power * 1.36, 1));
+    $power = floatval(number_format($power * 1.341, 1));
     return $power;
 }
