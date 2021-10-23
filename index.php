9e43baba0e11f16ccd29a6b4830a4d3f636827d8
antmar0417 <tonyboy@live.se>
2021-10-23 19:16:13 +0200

    added a dynamic site title

--- index.php
+++ index.php
@@ -1,24 +1,23 @@
 <?php require __DIR__ . '/header.php'; ?>
 <?php require __DIR__ . '/functions.php'; ?>
+<?php require __DIR__ . '/data.php'; ?>
 <main>
-    <section>
-        <h1 class="site-title">Cars.com</h1>
-        <p>Välj märke och bilmodell nedan.</p>
-        <div class="four-column-wrapper">
-            <p>
-                <a href="https://en.wikipedia.org/wiki/Big_cat">Audi</a>&nbsp;(1st)
-            </p>
-            <p>
-                <a href="https://en.wikipedia.org/wiki/Big_cat">Ford</a>&nbsp;(3st)
-            </p>
-            <p>
-                <a href="https://en.wikipedia.org/wiki/Big_cat">Volvo</a>&nbsp;(1st)
-            </p>
-            <p>
-                <a href="https://en.wikipedia.org/wiki/Big_cat">Volkswagen</a>&nbsp;(1st)
-            </p>
-        </div>
-    </section>
+    <h1 class="site-title"><?php echo $siteTitle; ?></h1>
+    <p>Välj märke och bilmodell nedan.</p>
+    <div class="four-column-wrapper">
+        <p>
+            <a href="https://en.wikipedia.org/wiki/Big_cat">Audi</a>&nbsp;(1st)
+        </p>
+        <p>
+            <a href="https://en.wikipedia.org/wiki/Big_cat">Ford</a>&nbsp;(3st)
+        </p>
+        <p>
+            <a href="https://en.wikipedia.org/wiki/Big_cat">Volvo</a>&nbsp;(1st)
+        </p>
+        <p>
+            <a href="https://en.wikipedia.org/wiki/Big_cat">Volkswagen</a>&nbsp;(1st)
+        </p>
+    </div>
     <h2>Bilar till salu</h2>
     <section class="cars">
         <?php foreach ($cars as $car) : ?>
@@ -27,11 +26,14 @@
                     <div class="image-wrapper">
                         <img src="<?php echo $car['image'] ?>" alt="<?php echo $car['make'] . $car['model'] ?>" />
                     </div>
-                    <ul>
-                        <li><?php echo $car['make'] . " " . $car['model'] ?></li>
-                        <li>Hästkrafter: <?php kwToHorsepower($car['kw']); ?></li>
-                        <p class="price">Pris: <?php echo $car['price'] ?></p>
-                    </ul>
+                    <div>
+                        <ul>
+                            <li><?php echo $car['make'] . " " . $car['model'] ?></li>
+                            <li>Färg: <?php echo $car['color'] ?></li>
+                            <li>Hästkrafter: <?php echo kwToHorsepower($car['kw']); ?></li>
+                        </ul>
+                        <p class="price">Pris: <?php echo $car['price'] ?> kr</p>
+                    </div>
                 </div>
             </div>
         <?php endforeach; ?>
