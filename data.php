a8d5158b4989e16fc215a39de157b6851e0c69db
antmar0417 <tonyboy@live.se>
2021-10-23 18:25:21 +0200

    changed the colors into cars in Swedish

--- data.php
+++ data.php
@@ -3,14 +3,14 @@
 declare(strict_types=1);
 
 $cars = [
-    ['make' => 'Ford', 'model' => 'Focus', 'Year' => 2005, 'kw' => 100, 'vinId' => 'JT164STJ200017216', 'color' => 'Blue', 'image' => 'images/Ford Focus.jpeg', 'price' => 15000],
-    ['make' => 'Volvo', 'model' => 'V70', 'Year' => 2008, 'kw' => 110, 'vinId' => 'JT164STJ210017216', 'color' => 'Silver', 'image' => 'images/Volvo V70.jpeg', 'price' => 70000],
-    ['make' => 'Ford', 'model' => 'Fiesta', 'Year' => 2010, 'kw' => 62, 'vinId' => 'JT164STJ220017216', 'color' => 'Red', 'image' => 'images/Ford Fiesta.jpeg', 'price' => 30000],
-    ['make' => 'Ford', 'model' => 'Mondeo', 'Year' => 2009, 'kw' => 120, 'vinId' => 'JT164STJ230017216', 'color' => 'Black', 'image' => 'images/Ford Mondeo.jpeg', 'price' => 35000],
-    ['make' => 'Ford', 'model' => 'F150', 'Year' => 2013, 'kw' => 202, 'vinId' => 'JT164STJ240017216', 'color' => 'Yellow', 'image' => 'images/Ford F150.jpeg', 'price' => 120000],
-    ['make' => 'Audi', 'model' => 'A4', 'Year' => 2018, 'kw' => 150, 'vinId' => 'JT164STJ250017216', 'color' => 'Green', 'image' => 'images/Audi A4.jpeg', 'price' => 90000],
-    ['make' => 'Volkswagen', 'model' => 'Tiguan', 'Year' => 2019, 'kw' => 121, 'vinId' => 'JT164STJ260017216', 'color' => 'Red', 'image' => 'images/VW Tiguan.jpeg', 'price' => 190000],
-    ['make' => 'Volkswagen', 'model' => 'Polo', 'Year' => 2018, 'kw' => 100, 'vinId' => 'JT164STJ270017216', 'color' => 'Blue', 'image' => 'images/VW Polo.jpeg', 'price' => 170000],
+    ['make' => 'Ford', 'model' => 'Focus', 'Year' => 2005, 'kw' => 100, 'vinId' => 'JT164STJ200017216', 'color' => 'Silver', 'image' => 'images/Ford Focus.jpeg', 'price' => 15000],
+    ['make' => 'Volvo', 'model' => 'V70', 'Year' => 2008, 'kw' => 110, 'vinId' => 'JT164STJ210017216', 'color' => 'Grå', 'image' => 'images/Volvo V70.jpeg', 'price' => 70000],
+    ['make' => 'Ford', 'model' => 'Fiesta', 'Year' => 2010, 'kw' => 62, 'vinId' => 'JT164STJ220017216', 'color' => 'Blå', 'image' => 'images/Ford Fiesta.jpeg', 'price' => 30000],
+    ['make' => 'Ford', 'model' => 'Mondeo', 'Year' => 2009, 'kw' => 120, 'vinId' => 'JT164STJ230017216', 'color' => 'Grå', 'image' => 'images/Ford Mondeo.jpeg', 'price' => 35000],
+    ['make' => 'Ford', 'model' => 'F150', 'Year' => 2013, 'kw' => 202, 'vinId' => 'JT164STJ240017216', 'color' => 'Grå', 'image' => 'images/Ford F150.jpeg', 'price' => 120000],
+    ['make' => 'Audi', 'model' => 'A4', 'Year' => 2018, 'kw' => 150, 'vinId' => 'JT164STJ250017216', 'color' => 'Vit', 'image' => 'images/Audi A4.jpeg', 'price' => 90000],
+    ['make' => 'Volkswagen', 'model' => 'Tiguan', 'Year' => 2019, 'kw' => 121, 'vinId' => 'JT164STJ260017216', 'color' => 'Grå', 'image' => 'images/VW Tiguan.jpeg', 'price' => 190000],
+    ['make' => 'Volkswagen', 'model' => 'Polo', 'Year' => 2018, 'kw' => 100, 'vinId' => 'JT164STJ270017216', 'color' => 'Vit', 'image' => 'images/VW Polo.jpeg', 'price' => 170000],
 ];
 
 $owners = [
