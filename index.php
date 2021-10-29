6e63d60c7698c5ec3f782599fde1ad8a6631a8de
antmar0417 <tonyboy@live.se>
2021-10-29 13:11:52 +0200

    used short echo tag

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 <?php require __DIR__ . '/functions.php'; ?>
 <?php require __DIR__ . '/data.php'; ?>
 <main>
-    <h1 class="site-title"><?php echo $siteTitle; ?></h1>
+    <h1 class="site-title"><?= $siteTitle; ?></h1>
     <p>Välj märke och bilmodell nedan.</p>
     <div class="four-column-wrapper">
         <?php foreach (array_unique(getCarBrand($cars)) as $make) : ?>
             <p>
-                <a href="models.php?make=<?php echo $make ?>"> <?php echo $make; ?> </a>
+                <a href="models.php?make=<?= $make ?>"> <?= $make; ?> </a>
             </p>
         <?php endforeach; ?>
     </div>
@@ -15,18 +15,18 @@
     <section class="cars">
         <?php foreach ($cars as $car) : ?>
             <div class="car">
-                <div class="two-column-wrapper" onclick="location.href='<?php echo $car['url']  ?>'">
+                <div class="two-column-wrapper" onclick="location.href='<?= $car['url']  ?>'">
                     <div class="image-wrapper">
-                        <img src="<?php echo $car['image'] ?>" alt="<?php echo $car['make'] . $car['model'] ?>" />
+                        <img src="<?= $car['image'] ?>" alt="<?= $car['make'] . $car['model'] ?>" />
                     </div>
                     <div>
                         <ul>
-                            <li><?php echo $car['make'] . " " . $car['model'] ?></li>
-                            <li>Mil: <?php echo $car['miles'] ?></li>
-                            <li>Årsmodell: <?php echo $car['year'] ?></li>
-                            <li>Hästkrafter: <?php echo kwToHorsepower($car['kw']); ?></li>
+                            <li><?= $car['make'] . " " . $car['model'] ?></li>
+                            <li>Mil: <?= $car['miles'] ?></li>
+                            <li>Årsmodell: <?= $car['year'] ?></li>
+                            <li>Hästkrafter: <?= kwToHorsepower($car['kw']); ?></li>
                         </ul>
-                        <p class="price">Pris: <?php echo $car['price'] ?> kr</p>
+                        <p class="price">Pris: <?= $car['price'] ?> kr</p>
                     </div>
                 </div>
             </div>
